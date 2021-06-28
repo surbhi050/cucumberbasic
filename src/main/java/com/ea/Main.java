f2f09bcd39e466faa5bd6d8ded0993ede11b869c
Surbhi Kumari <surbhikumari050@gmail.com>
2021-06-28 19:24:47 +0530

    adding comment

--- src/main/java/com/ea/Main.java
+++ src/main/java/com/ea/Main.java
@@ -1,7 +1,7 @@
 package com.ea;
 
 public class Main {
-// adding comment to check jenkins trigger
+// adding comment to check jenkins trigger through webhook
     public static void main(String[] args) {
 
 
